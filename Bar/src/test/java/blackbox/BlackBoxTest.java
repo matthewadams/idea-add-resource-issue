d7241288145a40231a956107e06f0af57d485fe8
Matthew Adams <matthewadams@northwesternmutual.com>
2018-06-10 13:21:13 -0500

    add resource test

--- Bar/src/test/java/blackbox/BlackBoxTest.java
+++ Bar/src/test/java/blackbox/BlackBoxTest.java
@@ -3,18 +3,26 @@ package blackbox;
 import dummy.DateTimeTestUtils;
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
 import java.time.Instant;
 import java.time.ZoneOffset;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BlackBoxTest {
 
     @Test
-    public void test() {
+    public void testSharedTestSource() {
         ZoneOffset expected =
                 ZoneOffset.ofHours(
                         DateTimeTestUtils.ET.getRules().isDaylightSavings(Instant.now()) ? -4 : -5);
         assertEquals(expected, DateTimeTestUtils.getCurrentEasternTimeZoneOffset());
+    }
+
+    @Test
+    public void testLoadResource() {
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("bar.json");
+        assertNotNull(stream);
     }
 }
