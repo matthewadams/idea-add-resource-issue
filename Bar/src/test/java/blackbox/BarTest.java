314bd797f530a13b7b1e78a34ff6d80a46d7bbf3
Matthew Adams <matthewadams@northwesternmutual.com>
2018-06-10 13:34:57 -0500

    rename tests

--- Bar/src/test/java/blackbox/BarTest.java
+++ Bar/src/test/java/blackbox/BarTest.java
@@ -14,7 +14,7 @@ import java.time.ZoneOffset;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class BlackBoxTest {
+public class BarTest {
 
     @Test
     public void testSharedTestSource() {
--- Foo/src/test/java/blackbox/FooTest.java
+++ Foo/src/test/java/blackbox/FooTest.java
@@ -13,7 +13,7 @@ import java.time.ZoneOffset;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class BlackBoxTest {
+public class FooTest {
 
     @Test
     public void testSharedTestSource() {
